b1cafc69b6c28c784f99093fd9367655d507ada2
humanheima <humanheima@gmail.com>
2017-05-06 10:17:01 +0800

    修改注册界面的逻辑

--- app/src/main/java/com/brotherd/poemtrip/activity/RegisterActivity.java
+++ app/src/main/java/com/brotherd/poemtrip/activity/RegisterActivity.java
@@ -2,13 +2,18 @@ package com.brotherd.poemtrip.activity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.view.View;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.brotherd.poemtrip.R;
 import com.brotherd.poemtrip.base.BaseActivity;
+import com.brotherd.poemtrip.impl.TextWatcherImpl;
 import com.brotherd.poemtrip.model.LoginModel;
 import com.brotherd.poemtrip.model.VerifyCodeModel;
 import com.brotherd.poemtrip.network.HttpResult;
@@ -43,6 +48,18 @@ public class RegisterActivity extends BaseActivity {
     TextView textGetVerify;
     @BindView(R.id.edit_password)
     EditText editPassword;
+    @BindView(R.id.text_phone_prefix)
+    TextView textPhonePrefix;
+    @BindView(R.id.view_split_phone)
+    View viewSplitPhone;
+    @BindView(R.id.img_clear_phone)
+    ImageView imgClearPhone;
+    @BindView(R.id.img_clear_verify_code)
+    ImageView imgClearVerifyCode;
+    @BindView(R.id.img_clear_password)
+    ImageView imgClearPassword;
+    @BindView(R.id.text_commit)
+    TextView textCommit;
 
     private String phone;
     private String verifyCode;
@@ -66,13 +83,83 @@ public class RegisterActivity extends BaseActivity {
         loadingDialog = new LoadingDialog(this);
     }
 
+    @Override
+    protected void bindEvent() {
+        editPhone.addTextChangedListener(new TextWatcherImpl() {
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (TextUtils.isEmpty(s)) {
+                    imgClearPhone.setVisibility(View.INVISIBLE);
+                    textGetVerify.setClickable(true);
+                    textGetVerify.setBackgroundColor(getResources().getColor(R.color.text_gray));
+                    textCommit.setClickable(false);
+                    textCommit.setBackgroundColor(getResources().getColor(R.color.alpha_green));
+                } else {
+                    imgClearPhone.setVisibility(View.VISIBLE);
+                    textGetVerify.setClickable(true);
+                    textGetVerify.setBackgroundColor(getResources().getColor(R.color.green));
+                    if (!TextUtils.isEmpty(editVerifyCode.getText()) && !TextUtils.isEmpty(editPassword.getText()) && checkProtocol.isChecked()) {
+                        textCommit.setClickable(true);
+                        textCommit.setBackgroundColor(getResources().getColor(R.color.green));
+                    }
+                }
+            }
+        });
+
+        editVerifyCode.addTextChangedListener(new TextWatcherImpl() {
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (TextUtils.isEmpty(s)) {
+                    imgClearVerifyCode.setVisibility(View.INVISIBLE);
+                    textCommit.setClickable(false);
+                    textCommit.setBackgroundColor(getResources().getColor(R.color.alpha_green));
+                } else {
+                    imgClearVerifyCode.setVisibility(View.VISIBLE);
+                    if (!TextUtils.isEmpty(editPhone.getText()) && !TextUtils.isEmpty(editPassword.getText()) && checkProtocol.isChecked()) {
+                        textCommit.setClickable(true);
+                        textCommit.setBackgroundColor(getResources().getColor(R.color.green));
+                    }
+                }
+            }
+        });
+
+        editPassword.addTextChangedListener(new TextWatcherImpl() {
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (TextUtils.isEmpty(s)) {
+                    imgClearPassword.setVisibility(View.INVISIBLE);
+                    textCommit.setClickable(false);
+                    textCommit.setBackgroundColor(getResources().getColor(R.color.alpha_green));
+                } else {
+                    imgClearPassword.setVisibility(View.VISIBLE);
+                    if (!TextUtils.isEmpty(editPhone.getText()) && !TextUtils.isEmpty(editVerifyCode.getText()) && checkProtocol.isChecked()) {
+                        textCommit.setClickable(true);
+                        textCommit.setBackgroundColor(getResources().getColor(R.color.green));
+                    }
+                }
+            }
+        });
+
+        checkProtocol.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (!isChecked) {
+                    textCommit.setClickable(false);
+                    textCommit.setBackgroundColor(getResources().getColor(R.color.alpha_green));
+                } else {
+                    if (!TextUtils.isEmpty(editPhone.getText()) && !TextUtils.isEmpty(editVerifyCode.getText()) && !TextUtils.isEmpty(editPassword.getText())) {
+                        textCommit.setClickable(true);
+                        textCommit.setBackgroundColor(getResources().getColor(R.color.green));
+                    }
+                }
+            }
+        });
+
+    }
+
     @OnClick(R.id.text_get_verify)
     public void getVerify() {
         phone = editPhone.getText().toString();
-        if (TextUtils.isEmpty(phone)) {
-            Toast.showToast(this, getString(R.string.phone_cant_null));
-            return;
-        }
         if (!RegularUtil.checkPhone(phone)) {
             Toast.showToast(this, getString(R.string.phone_pattern_error));
             return;
@@ -125,22 +212,8 @@ public class RegisterActivity extends BaseActivity {
     @OnClick(R.id.text_commit)
     public void commit() {
         phone = editPhone.getText().toString();
-        if (TextUtils.isEmpty(phone)) {
-            Toast.showToast(this, getString(R.string.phone_cant_null));
-            return;
-        }
         if (!RegularUtil.checkPhone(phone)) {
             Toast.showToast(this, getString(R.string.phone_pattern_error));
-            return;
-        }
-        verifyCode = editVerifyCode.getText().toString();
-        if (TextUtils.isEmpty(verifyCode)) {
-            Toast.showToast(this, getString(R.string.verify_code_cant_null));
-            return;
-        }
-        password = editPassword.getText().toString();
-        if (TextUtils.isEmpty(password)) {
-            Toast.showToast(this, getString(R.string.password_cant_null));
             return;
         }
         loadingDialog.show();
@@ -174,6 +247,24 @@ public class RegisterActivity extends BaseActivity {
     }
 
     @OnClick(R.id.text_protocol)
-    public void onTextProtocolClicked() {
+    public void seeProtocol() {
+    }
+
+    @OnClick({R.id.img_clear_phone, R.id.img_clear_verify_code, R.id.img_clear_password})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.img_clear_phone:
+                editPhone.setText("");
+                imgClearPhone.setVisibility(View.INVISIBLE);
+                break;
+            case R.id.img_clear_verify_code:
+                editVerifyCode.setText("");
+                imgClearVerifyCode.setVisibility(View.INVISIBLE);
+                break;
+            case R.id.img_clear_password:
+                editPassword.setText("");
+                imgClearPassword.setVisibility(View.INVISIBLE);
+                break;
+        }
     }
 }
